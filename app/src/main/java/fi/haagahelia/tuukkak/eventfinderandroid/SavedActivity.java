ad409d426de422085cebab6fead5ccf3c28ea6a3
tuukka26 <32544951+tuukka26@users.noreply.github.com>
2018-06-06 17:53:54 +0300

    Made the opening screen plus buttons to navigate to search and saved events activities

--- app/src/main/java/fi/haagahelia/tuukkak/eventfinderandroid/SavedActivity.java
+++ app/src/main/java/fi/haagahelia/tuukkak/eventfinderandroid/SavedActivity.java
@@ -1,16 +1,13 @@
 package fi.haagahelia.tuukkak.eventfinderandroid;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class SavedActivity extends AppCompatActivity {
 
     // Create menu
     @Override
@@ -25,11 +22,11 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
         switch (item.getItemId()) {
-            case R.id.menu_item_search:
-                Intent intentAdd = new Intent(this, SearchActivity.class);
+            case R.id.menu_item_home:
+                Intent intentAdd = new Intent(this, MainActivity.class);
                 startActivityForResult(intentAdd, 0);
                 return true;
-            case R.id.menu_item_saved:
+            case R.id.menu_item_search:
                 Intent intentInstructions = new Intent(this, SavedActivity.class);
                 startActivityForResult(intentInstructions, 0);
                 return true;
@@ -41,27 +38,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // Set buttons and their onClickListeners for navigating between Activities
-        Button btnSearch = findViewById(R.id.btnSearch);;
-        Button btnSave = findViewById(R.id.btnSave);
-
-        btnSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SavedActivity.class);
-                        startActivity(intent);
-            }
-        });
-
+        setContentView(R.layout.activity_saved);
     }
 }
