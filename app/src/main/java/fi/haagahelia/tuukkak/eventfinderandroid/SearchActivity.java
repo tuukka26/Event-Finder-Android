9c226189cd28d47cdab3d019dd8a2e520a47a327
tuukka26 <32544951+tuukka26@users.noreply.github.com>
2018-06-12 12:05:31 +0300

    Created the method to fetch and parse the JSON response from Eventful API succesfully. Need to style the page still

--- app/src/main/java/fi/haagahelia/tuukkak/eventfinderandroid/SearchActivity.java
+++ app/src/main/java/fi/haagahelia/tuukkak/eventfinderandroid/SearchActivity.java
@@ -1,13 +1,48 @@
 package fi.haagahelia.tuukkak.eventfinderandroid;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class SearchActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    ListView tvResults;
+    EditText etLocation;
+    EditText etKeyword;
+
+    ArrayList<HashMap<String, String>> eventsList;
+
+    String appKey = "APP_KEY";
+    String date = "All";
+
 
     // Create menu
     @Override
@@ -39,5 +74,144 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        progressBar = findViewById(R.id.progressBar);
+        tvResults =  findViewById(R.id.tvResults);
+        etLocation = findViewById(R.id.etLocation);
+        etKeyword = findViewById(R.id.etKeyword);
+        eventsList = new ArrayList<>();
+
+        // Set search button's onClickListener
+        ImageButton btnSearch = findViewById(R.id.btnSearch);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new FetchEvents().execute();
+            }
+        });
+
+    }
+
+    //Fetch and parse the JSON response from Eventful API
+    class FetchEvents extends AsyncTask<Void, Void, String> {
+
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(Void... urls) {
+            String location = etLocation.getText().toString();
+            String keyword = etKeyword.getText().toString();
+            eventsList.clear();
+
+            try{
+                URL url = new URL("http://api.eventful.com/json/events/search?app_key=" + appKey + "&keywords=" + keyword + "&where=" + location + "&date=" + date + "&page_size=20&sort_order=popularity&change_multi_day_start=true");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("/n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally {
+                    urlConnection.disconnect();
+                }
+            }
+            catch (Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                showToast(e.getMessage());
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            if(response == null) {
+                showToast("An error occurred, please try again");
+            }
+            progressBar.setVisibility(View.GONE);
+            Log.i("INFO", response);
+
+            try {
+                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+                JSONObject eventsObj = object.getJSONObject("events");
+                JSONArray eventsArr = eventsObj.getJSONArray("event");
+
+               for (int i = 0; i < eventsArr.length(); i++) {
+                    try {
+                        JSONObject newObj = eventsArr.getJSONObject(i);
+                        String title = newObj.getString("title");
+                        String venue = newObj.getString("venue_name");
+                        String start_time = newObj.getString("start_time");
+                        String venue_address = newObj.getString("venue_address");
+                        String city = newObj.getString("city_name");
+
+                        HashMap<String, String> event = new HashMap<>();
+
+                        event.put("title", title + " @ " + venue);
+                        event.put("start_time", "Date: " + start_time);
+                        event.put("venue_address", "Venue address: " + venue_address + ", " + city);
+
+                        eventsList.add(event);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        showToast(e.getMessage());
+                    }
+                }
+
+                ListAdapter adapter = new SimpleAdapter(SearchActivity.this, eventsList, R.layout.list_item, new String[] { "title", "start_time", "venue_address" },
+                        new int[] { R.id.title, R.id.start_time, R.id.venue_address });
+                        tvResults.setAdapter(adapter);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+        }
+
+        //Change date depending on the radio button selection
+        public void onRadioButtonClicked (View view) {
+
+            boolean checked = ((RadioButton) view).isChecked();
+
+            switch(view.getId()) {
+                case R.id.rbToday:
+                    if (checked)
+                        date = "Today";
+                    break;
+                case R.id.rbThisWeek:
+                    if (checked)
+                        date = "Next week";
+                    break;
+                case R.id.rbNextWeek:
+                    if (checked)
+                        date = "This week";
+                    break;
+            }
+
+        }
+
+    public void showToast(String text) {
+        int time = Toast.LENGTH_LONG;
+        Context context = getApplicationContext() ;
+        Toast toast = Toast.makeText(context, text, time);
+        toast.show();
     }
 }
+
+
+
+  /*  public void showToast(String text) {
+        int time = Toast.LENGTH_LONG;
+        Context context = getApplicationContext() ;
+        Toast toast = Toast.makeText(context, text, time);
+        toast.show();
+    } */
+
